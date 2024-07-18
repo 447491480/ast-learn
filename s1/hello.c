e11c0ce21126be029cbc580c5e867184a86c4683
livecopilot <changwj@geovis.com.cn>
2024-07-18 13:46:08 +0800

    update

--- s1/hello.c
+++ s1/hello.c
@@ -1,16 +1,19 @@
-#include <stdio.h> 
-int main(int argc,char *argv[])
+#include <stdio.h>
+int main(int argc, char *argv[])
 {
-    int age=45;
+    int age = 45;
     if (age >= 17 + 8 + 20)
     {
         printf("Hello old man!\\n");
-        
     }
     else
     {
         printf("Hello young man!\\n");
-        
     }
     return 0;
+}
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
 }
--- s1/readme.md
+++ s1/readme.md
@@ -1,3 +1,9 @@
 ## js ast 分析网址
 
 https://www.jointjs.com/demos/abstract-syntax-tree
+
+## clang 导出ast
+```bash
+clang -cc1 -ast-dump hello.c
+```
+
